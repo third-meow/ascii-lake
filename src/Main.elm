f870a87cc1b36aef67c6758ce4dff42f03061f78
The third meow <jsheaandcats@gmail.com>
2018-12-07 22:57:11 +1300

    implement proof-of-concept

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,4 @@
+import Array
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
@@ -6,15 +7,29 @@ import Html.Events exposing (onClick)
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
+symbols = (Array.fromList ["-", "\\", "|", "/", "_", "+", "~"])
+
+
+checkSymb : Maybe String -> String
+checkSymb maybeSymb =
+  case maybeSymb of
+    Just value ->
+      value
+    Nothing ->
+      "E"
+
+getSymb : Int -> String
+getSymb index =
+  checkSymb (Array.get index symbols)
 
 -- MODEL
 
-type alias Model = { symb: String }
+type alias Model = { state : Int}
+
 
 init : Model
 init =
-  { symb = "-" }
-
+  { state = 0 }
 
 -- UPDATE
 
@@ -24,14 +39,13 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Switch ->
-      { model | symb = "|" }
-
+      { model | state = modBy (Array.length symbols) (model.state + 1) }
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Switch] [ text "switch" ]
-    , div [] [text model.symb]
+    [ button [ onClick Switch ] [ text "switch" ]
+    , div [] [ text (getSymb model.state) ]
     ]
